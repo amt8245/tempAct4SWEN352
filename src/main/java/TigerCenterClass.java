94bad0b6029d0d67755d126470a06d481445cda1
aids <amt8245@rit.edu>
2023-11-16 17:21:11 -0500

    Initial commit

---
+++ src/main/java/TigerCenterClass.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+class TigerCenterClass {
+    private WebDriver driver;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Browser browser = Browser.FIREFOX; // Can be changed to Browser.CHROME
+        driver = browser.setUpDriver();
+        baseUrl = "https://"; // TARGET URL
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testClassSearchButton() throws Exception {
+        driver.get(baseUrl+"tigercenter.rit.edu/tigerCenterApp/landing");
+        WebElement classButton = driver.findElement(By.xpath("//*[@id=\"angularApp\"]/app-root/div[2]/mat-sidenav-container[2]/mat-sidenav-content/div[2]/landing-page/div/div/div/div/div[4]/a[1]"));
+        assertEquals("Class Search", classButton.getText());
+        classButton.click();
+    }
+
+    private enum Browser {
+        CHROME("webdriver.chrome.driver",
+                "chromedriver",
+                ChromeDriver::new),
+        FIREFOX("webdriver.gecko.driver",
+                "geckodriver",
+                FirefoxDriver::new);
+
+        private final String driverPropertyKey;
+        private final String driverBaseName;
+        private final Supplier<WebDriver> webDriverSupplier;
+
+        Browser(String driverPropertyKey,
+                String driverBaseName,
+                Supplier<WebDriver> webDriverSupplier) {
+            this.driverPropertyKey = driverPropertyKey;
+            this.driverBaseName = driverBaseName;
+            this.webDriverSupplier = webDriverSupplier;
+        }
+
+        private WebDriver setUpDriver() {
+            String driverName = this.driverBaseName;
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                driverName += ".exe";
+            }
+            System.setProperty(this.driverPropertyKey, driverName);
+            return this.webDriverSupplier.get();
+        }
+    }
+}
